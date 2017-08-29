cb0594dea53926303d4eca7bd9373b7c0bd2cd8e
emhar <emmanuel.harleaux@gmail.com>
2017-08-29 11:57:23 +0200

    Fix jms deserialisation exception type
    This exception must return a 400 code

--- Serializer/JMS/EnumHandler.php
+++ Serializer/JMS/EnumHandler.php
@@ -4,6 +4,7 @@ namespace Fervo\EnumBundle\Serializer\JMS;
 
 use Fervo\EnumBundle\Serializer\AbstractEnumNormalizer;
 use JMS\Serializer\Context;
+use JMS\Serializer\Exception\RuntimeException;
 use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\VisitorInterface;
 use Symfony\Component\Serializer\Exception\InvalidArgumentException;
@@ -35,7 +36,7 @@ class EnumHandler extends AbstractEnumNormalizer
             try {
                 return $this->denormalizeEnum($data, $type['name']);
             } catch (UnexpectedValueException $e) {
-                throw new \RuntimeException($e->getMessage());
+                throw new RuntimeException($e->getMessage());
             }
         }
 
